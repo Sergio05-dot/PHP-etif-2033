2b4cfda67f4f18e6db50a7acc4583403fc6dac73
Sergio05-dot <sergiohcarvalho05@gmail.com>
2023-05-04 17:28:55 +0200

    constantes, prints y echos

---
+++ ejercicio-echo.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h2> ¡PHP es divertido! </h2>";
+
+echo "¡Hola mundo! <br>";
+echo "<br>";
+echo "¡Estoy a punto de aprender PHP! <br>";
+echo "<br>";
+echo "Esta", " cadena de texto ", "se ha", " creado ", " con multiples parámetros.";
+
+?>
+<br>
+<a href="ejercicio-print.php">ejercicio de print</a>
---
+++ ejercicio-print.php
@@ -0,0 +1,10 @@
+<?php
+$txt1 = "Aprende PHP";
+$txt2 = "pr0j3ct.com";
+$x = 5; $y = 4;
+print "<h2>" . $txt1 . "</h2>";
+print "Estudia PHP con " . $txt2 . "<br>";
+print $x + $y; //9
+?>
+<br>
+<a href="ejercicio-echo.php">Ejercicio de echo</a>
